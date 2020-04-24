a5a3cec149d796406c7a9c916e6e2a6fdbfcf38d
Wekiban <wes.banyard@googlemail.com>
2020-04-24 11:30:45 +0200

    added render function

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 let bookshelf = document.getElementById('bookshelf');
 let myLibrary = [];
 
+
 function Book(title, author, pages, status){
  this.title = title;
  this.author = author;
  this.pages = pages;
  this.status = status;
 }
+
 
 Book.prototype.info = function(){return `${this.title} by ${this.author}, ${this.pages} pages, ${this.status}`}
 
@@ -15,17 +17,27 @@ return myLibrary.push(new Book(title, author, pages, status))
 }
 
 
-
 addBookToLibrary('The Hobbit', 'J.R.R Tolkien', 295, 'Not Read');
-addBookToLibrary('The Hobbit', 'J.R.R Tolkien', 295, 'Not Read');
+addBookToLibrary('Mindfulness in Plain English', 'Henepola Gunaratana', 222, 'Not Read');
 
 
 
-let render = function(){
+function render(){
     myLibrary.forEach(book => {
-        console.log(book.title, book.author, book.pages, book.status)
+        var card = document.createElement("div")
+        var title = document.createElement("h2");
+        var author = document.createElement("p");
+        var pages = document.createElement("p");
+        var status = document.createElement("p");
+        title.innerHTML = book.title;
+        author.innerHTML = book.author;
+        pages.innerHTML = book.pages;
+        status.innerHTML = book.status;
+        card.appendChild(title);
+        card.appendChild(author);
+        card.appendChild(pages);
+        card.appendChild(status);
+        bookshelf.appendChild(card);
     });
-}
-
-
-render();
+} 
+ render();
