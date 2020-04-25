308bbf2f3fd654564abde29239044528e8668f96
Wekiban <wes.banyard@googlemail.com>
2020-04-25 21:14:11 +0200

    almost completed may make some more changes

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let bookshelf = document.getElementById('bookshelf');
 let myLibrary = [];
+let colors = ['#03CEA4', '#345995', '#FB4D3D', '#CA1551']
+let submit = document.getElementById('submit');
+let form = document.getElementById('book-form')
 function Book(title, author, pages, status){
  this.title = title;
  this.author = author;
@@ -18,7 +21,7 @@ function addBookToLibrary(title, author, pages, status){
 
 function removeBookLibrary(id){
     var thisBook = document.getElementById(id);
-    myLibrary.splice(myLibrary.findIndex(x => x.title === id));
+    myLibrary.splice(myLibrary.findIndex(book => book.title === id), 1);
     thisBook.remove();
 }
 
@@ -30,14 +33,31 @@ function render(book){
         var author = document.createElement("p");
         var pages = document.createElement("p");
         var status = document.createElement("p");
-        var removeButton = document.createElement('button');
+        var removeButton = document.createElement("i");
+        var readTick = document.createElement("i");
+        status.setAttribute("class", "status");
+        readTick.addEventListener('click', function(e){
+          if(e.target.style.color === 'white'){
+            myLibrary.find(x => x.title === book.title).status = "I've not read this!"
+            status.innerHTML = "I've not read this!"
+            e.target.style.color = 'grey'
+          } else {
+            myLibrary.find(x => x.title === book.title).status = "I've read this!"
+            e.target.style.color = 'white';
+            status.innerHTML = "I've read this!"
+          }
+        })
+
+        bookContainer.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
         bookContainer.setAttribute("id", book.title);
         bookContainer.classList.add('book')
         title.innerHTML = book.title;
         author.innerHTML = book.author;
-        pages.innerHTML = book.pages;
+        pages.innerHTML = `${book.pages} pages`;
         status.innerHTML = book.status;
-        removeButton.textContent = 'Remove Book';
+        readTick.classList.add("fas", "fa-check", "read-tick")
+        readTick.style.color = book.status === "I've read this!" ? "white" : "grey";
+        removeButton.classList.add("fas", "fa-trash-alt", "remove-button")
         removeButton.addEventListener('click',function(e){
             let bookId = e.target.parentNode.id;
             removeBookLibrary(bookId);
@@ -47,16 +67,17 @@ function render(book){
         bookContainer.appendChild(pages);
         bookContainer.appendChild(status);
         bookContainer.appendChild(removeButton);
+        bookContainer.appendChild(readTick);
         bookshelf.appendChild(bookContainer); 
         
 } 
  
-addBookToLibrary('The Hobbit', 'J.R.R Tolkien', '295 pages', 'Not Read');
-addBookToLibrary('Mindfulness in Plain English', 'Henepola Gunaratana', '222 pages', 'Not Read');
-addBookToLibrary('The Hobbit', 'J.R.R Tolkien', '295 pages', 'Not Read');
-addBookToLibrary('Mindfulness in Plain English', 'Henepola Gunaratana', '222 pages', 'Not Read');
-addBookToLibrary('The Hobbit', 'J.R.R Tolkien', '295 pages', 'Not Read');
-addBookToLibrary('Mindfulness in Plain English', 'Henepola Gunaratana', '222 pages', 'Not Read');
+addBookToLibrary('The Hobbit', 'J.R.R Tolkien', 295, "I've read this!");
+addBookToLibrary('Mindfulness in Plain English', 'Henepola Gunaratana', 439, "I've read this!");
+addBookToLibrary('James Bond', 'Ian Fleming', 225, "I've not read this!");
+addBookToLibrary('The Curious Incident of the Dog in the Night-Time', 'Mark Haddon', 274, "I've read this!");
+addBookToLibrary('1989', 'George Orwell', 215, "I've read this!");
+addBookToLibrary('In Search of Lost Time', 'Marcel Proust', 237, "I've read this!");
 
 var modal = document.getElementById("myModal");
 
@@ -74,11 +95,26 @@ btn.onclick = function() {
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
+  form.reset();
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
     modal.style.display = "none";
+    form.reset();
   }
 }
+
+submit.addEventListener('click', function(){
+  let title = document.getElementById('title-input').value.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+  let author = document.getElementById('author-input').value.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});;
+  let pages = document.getElementById('pages-input').value;
+  let status = document.getElementById('status').checked ? "Ive read this!" : "Ive not read this!";
+  if(myLibrary.find(x => x.title === title)) return alert('Library already contains this book');
+  if(!title) return alert('Cannot submit book to library Without title');
+  addBookToLibrary(title, author, pages, status);
+  form.reset();
+  modal.style.display = "none";
+})
+
